c17937006a700f54a0dc12b5903160af67cd695d
Barry Gray <barrygray@the-grays-time-capsule.gateway.pace.com>
2014-04-04 15:28:44 -0700

    added a test class

---
+++ Git Test/ANTest.h
@@ -0,0 +1,13 @@
+//
+//  ANTest.h
+//  Git Test
+//
+//  Created by Barry Gray on 4/4/14.
+//  Copyright (c) 2014 Barry Gray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ANTest : NSObject
+
+@end
---
+++ Git Test/ANTest.m
@@ -0,0 +1,13 @@
+//
+//  ANTest.m
+//  Git Test
+//
+//  Created by Barry Gray on 4/4/14.
+//  Copyright (c) 2014 Barry Gray. All rights reserved.
+//
+
+#import "ANTest.h"
+
+@implementation ANTest
+
+@end
