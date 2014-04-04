4d2dde5ff8a4e646e96176d9ca6fb685899ce13b
Barry Gray <barrygray@the-grays-time-capsule.gateway.pace.com>
2014-04-04 14:55:24 -0700

    Initial Commit

---
+++ Git Test/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  Git Test
+//
+//  Created by Barry Gray on 4/4/14.
+//  Copyright (c) 2014 Barry Gray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/ANViewController.h
@@ -0,0 +1,13 @@
+//
+//  ANViewController.h
+//  Git Test
+//
+//  Created by Barry Gray on 4/4/14.
+//  Copyright (c) 2014 Barry Gray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANViewController : UIViewController
+
+@end
